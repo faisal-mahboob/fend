d4cffbcceb6358c692a2b907c1336ce12b41e773
Faisal Mahboob <81893782+faisal-mahboob@users.noreply.github.com>
2021-08-18 17:34:33 -0400

    changes
    introduced some sass and made production work , using loaders.

--- projects/evaluate-news-nlp/webpack.dev.js
+++ projects/evaluate-news-nlp/webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+//const loader = require('sass-loader')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -11,9 +12,14 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+
+            {
+                test: /\.scss$/,
+                use:['style-loader','css-loader','sass-loader']
             }
         ]
     },
